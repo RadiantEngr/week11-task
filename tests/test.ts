b713de486e98c7245a4d95ce0bd9098eae439f23
RadiantEngr <onaolapo.ak@gmail.com>
2020-09-29 20:46:01 +0100

    testing circleCI

--- tests/test.ts
+++ tests/test.ts
@@ -1,4 +1,3 @@
-// import app from '../app';
 import "../server/schema/schema";
 import "../src/schema/schema";
 import { Organization } from "../server/models/organization";
@@ -10,27 +9,40 @@ import { MongoMemoryServer } from "mongodb-memory-server";
 import mongoose from "mongoose";
 import { graphql } from "graphql";
 
-// const request = supertest(app);
-const mongodb = new MongoMemoryServer();
+const mongod = new MongoMemoryServer();
+
+const closeDatabase = async () => {
+  await mongoose.connection.dropDatabase();
+  await mongoose.connection.close();
+  await mongod.stop();
+};
+const clearDatabase = async () => {
+  const collections = mongoose.connection.collections;
+  for (const key in collections) {
+    const collection = collections[key];
+    await collection.deleteMany({}, () => {});
+  }
+};
 
 let server;
 
-describe("/graphql", () => {
+describe("/graphql1", () => {
   beforeAll(() => {
     server = require("../app");
   });
   const request = supertest(server);
+
+  afterEach(async () => clearDatabase());
+
   afterAll(() => {
     server.close();
   });
 
-  
   describe("All Test Suites", () => {
-  
     it("can fetch all organizations", async () => {
       try {
         request
-          .post("/graphql")
+          .post("/graphql1")
           .send({
             query: "{organizations{id, organization, ceo}}",
           })
@@ -46,39 +58,60 @@ describe("/graphql", () => {
       }
     });
 
-    // it("can fetch an organization", async () => {
-    //     request
-    //       .post('/graphql')
-    //       .send({
-    //         query:
-    //           '{ organization(id: "5f732aa663bce128ebd3dd4a") { id, organization, country } }',
-    //       })
-    //       .set("Accept", "application.json")
-    //       .expect("Content-Type", /json/)
-    //       .expect(200)
-    //       .end((err, res) => {
-    //         if (err) return err.message;
-    //         console.log(res.body);
-    //         const val = res.body.data.organization;
-    //         expect(val).toHaveProperty("_id")
-    //         expect(val).toHaveProperty("organization");
-    //       });
-    //   });
+    it("can fetch an organization", async () => {
+      request
+        .post("/graphql1")
+        .send({
+          query:
+            '{ organization(id: "5f732aa663bce128ebd3dd4a") { id, organization, country } }',
+        })
+        .set("Accept", "application.json")
+        .expect("Content-Type", /json/)
+        .expect(200)
+        .end((err, res) => {
+          if (err) return err.message;
+          console.log(res.body);
+          const val = res.body.data.organization;
+          expect(val).toHaveProperty("_id");
+          expect(val).toHaveProperty("organization");
+        });
+    });
 
-    // it("can add to organizations", () => {
+    it("can add to organizations", () => {
+      request
+        .post("/graphql1")
+        .send({
+          query:
+            'mutation {addOrganization(organization:"Pito Bank",products: ["Transfer", "Withdrawal"],marketValue: "92%", address: "Lacagan", ceo:"Mr Abass", country: "Nigeria", employees: ["Bisi", "Kenny", "Silver"]){organization, products}}',
+        })
+        .set("Accept", "application.json")
+        .expect("Content-Type", /json/)
+        .expect(200)
+        .end((err, res) => {
+          console.log(res);
+          if (err) return err.message;
+
+          const val = res.body.data.addOrganization;
+          expect(val).toHaveProperty("organization");
+          expect(val).toHaveProperty("ceo");
+          expect(val).toHaveProperty("marketValue");
+        });
+    });
+
+    // it("can update organization", () => {
     //   request
     //     .post("/graphql")
     //     .send({
     //       query:
-    //         'mutation {addOrganization(organization:"Pito Bank",products: ["Transfer", "Withdrawal"],marketValue: "92%", address: "Lacagan", ceo:"Mr Abass", country: "Nigeria", employees: ["Bisi", "Kenny", "Silver"]){organization, products}}',
+    //         'mutation {updateOrganization(organization:"Pito Bank",products: ["first", "second"],marketValue: "92%", address: "Lacagan", ceo:"Mr Abass", country: "Nigeria", employees: ["Bisi", "Kenny", "Silver"]){organization, products}}',
     //     })
     //     .set("Accept", "application.json")
     //     .expect("Content-Type", /json/)
     //     .expect(200)
     //     .end((err, res) => {
     //       console.log(res);
-    //       if (err) return err.message;  
-        
+    //       if (err) return err.message;
+    //     expect(res.body).toBeInstanceOf{Object}
     //       const val = res.body.data.addOrganization;
     //       expect(val).toHaveProperty("organization");
     //       expect(val).toHaveProperty("ceo");
@@ -156,6 +189,5 @@ describe("/graphql", () => {
     it("can get all staff", async () => {
       expect(async () => await Staffs.findByIdAndRemove()).not.toThrow();
     });
-    
   });
 });
