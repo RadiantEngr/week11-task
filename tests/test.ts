f504976845b416fd1bd268a98555de1b05294777
RadiantEngr <onaolapo.ak@gmail.com>
2020-09-30 10:03:25 +0100

    testing circleCI

--- tests/test.ts
+++ tests/test.ts
@@ -7,9 +7,8 @@ import { Staffs } from "../src/models/staffs";
 import supertest from "supertest";
 import { MongoMemoryServer } from "mongodb-memory-server";
 import mongoose from "mongoose";
-import app from "../app"
+import app from "../app";
 import { graphql } from "graphql";
-import { doesNotMatch } from "assert";
 
 const mongod = new MongoMemoryServer();
 
@@ -29,182 +28,184 @@ const clearDatabase = async () => {
 let server;
 
 // describe("/graphql1", () => {
-  // beforeAll(() => {
-  //   server = require("../app");
+beforeAll(() => {
+  server = require("../app");
+});
+const request = supertest(app);
+
+afterEach(async () => clearDatabase());
+
+afterAll(() => {
+  // server.close();
+  closeDatabase();
+});
+
+describe("All Test Suites", () => {
+  it("can fetch all organizations", async (done) => {
+    request
+      .post("/graphql1")
+      .send({
+        query: "{organizations{id, organization, ceo}}",
+      })
+      .set("Accept", "application/json")
+      .expect("Content-Type", /json/)
+      .expect(200)
+      .end(function (err, res) {
+        if (err) done("the error", err.message);
+        // console.log("the res",res.body)
+        expect(res.body).toBeInstanceOf(Object);
+        done();
+      });
+  });
+
+  // it("can fetch an organization", async (done) => {
+  //   try {
+  //   request
+  //     .post("/graphql1")
+  //     .send({
+  //       query:
+  //         '{ organization(id: "5f732aa663bce128ebd3dd4a") { id, organization, country } }',
+  //     })
+  //     .set("Accept", "application.json")
+  //     .expect("Content-Type", /json/)
+  //     .expect(200)
+  //     .end((err, res) => {
+  //       if (err) done(err);
+  //       const val = res.body.data.organization;
+  //       expect(val).toHaveProperty("_id");
+  //       expect(val).toHaveProperty("organization");
+  //       done()
+  //     });
+  //   } catch (err) {
+  //     console.error(err.message);
+  //     done()
+  //   }
   // });
-  const request = supertest(app);
 
-  afterEach(async () => clearDatabase());
+  it("can add to organizations", async (done) => {
+    request
+      .post("/graphql1")
+      .send({
+        query:
+          'mutation {addOrganization(organization:"Pito Bank",products: ["Transfer", "Withdrawal"],marketValue: "92%", address: "Lacagan", ceo:"Mr Abass", country: "Nigeria", employees: ["Bisi", "Kenny", "Silver"]){organization, products, ceo}}',
+      })
+      .set("Accept", "application.json")
+      .expect("Content-Type", /json/)
+      .expect(200)
+      .end((err, res) => {
+        if (err) done(err);
+        const val = res.body.data.addOrganization;
+        expect(val).toHaveProperty("organization");
+        expect(val).toHaveProperty("ceo");
+        // expect(val).toHaveProperty("marketValue");
+        done();
+      });
+  });
+});
+// it("can update organization", () => {
+//   try {
+//   request
+//     .post("/graphql")
+//     .send({
+//       query:
+//         'mutation {updateOrganization(organization:"Pito Bank",products: ["first", "second"],marketValue: "92%", address: "Lacagan", ceo:"Mr Abass", country: "Nigeria", employees: ["Bisi", "Kenny", "Silver"]){organization, products}}',
+//     })
+//     .set("Accept", "application.json")
+//     .expect("Content-Type", /json/)
+//     .expect(200)
+//     .end((err, res) => {
+//       console.log(res);
+//       if (err) return err.message;
+//     expect(res.body).toBeInstanceOf{Object}
+//       const val = res.body.data.addOrganization;
+//       expect(val).toHaveProperty("organization");
+//       expect(val).toHaveProperty("ceo");
+//       expect(val).toHaveProperty("marketValue");
+//     });
+//   } catch (error) {
+//     console.log(`error ${error.toString()}`);
+//   }
+// });
 
-  
-  describe("All Test Suites",  () => {
-    it("can fetch all organizations", async (done) => {
-        request
-          .post("/graphql1")
-          .send({
-            query: "{organizations{id, organization, ceo}}",
-          })
-          .set("Accept", "application/json")
-          .expect("Content-Type", /json/)
-          .expect(200)
-          .end(function (err, res) {
-            if (err) done("the error",err.message) 
-            // console.log("the res",res.body)
-            expect(res.body).toBeInstanceOf(Object);
-            done()
-          }) 
-    })
+//   it("can add a purchased car", async () => {
+//     let data = {
+//       type: "Toyota",
+//       modelNumber: "7452MXDGH-2017",
+//       saleDate: "2020-09-10T14:14:08.670Z",
+//       buyer: "Olapeju",
+//       color: "Green",
+//     };
 
-    // it("can fetch an organization", async (done) => {
-    //   try {
-    //   request
-    //     .post("/graphql1")
-    //     .send({
-    //       query:
-    //         '{ organization(id: "5f732aa663bce128ebd3dd4a") { id, organization, country } }',
-    //     })
-    //     .set("Accept", "application.json")
-    //     .expect("Content-Type", /json/)
-    //     .expect(200)
-    //     .end((err, res) => {
-    //       if (err) done(err);
-    //       console.log(res.body);
-    //       const val = res.body.data.organization;
-    //       expect(val).toHaveProperty("_id");
-    //       expect(val).toHaveProperty("organization");
-    //       done()
-    //     });
-    //   } catch (err) {
-    //     console.error(err.message);
-    //     done()
-    //   }
-    // });
+//     expect(async () => await PurchasedCars.create()).not.toThrow();
+//   });
 
-    it("can add to organizations", async (done) => {
-  
-      request
-        .post("/graphql1")
-        .send({
-          query:
-            'mutation {addOrganization(organization:"Pito Bank",products: ["Transfer", "Withdrawal"],marketValue: "92%", address: "Lacagan", ceo:"Mr Abass", country: "Nigeria", employees: ["Bisi", "Kenny", "Silver"]){organization, products, ceo}}',
-        })
-        .set("Accept", "application.json")
-        .expect("Content-Type", /json/)
-        .expect(200)
-        .end((err, res) => {
-          console.log(res);
-          if (err) done(err);
+//   it("can add to cars", async () => {
+//     let data = {
+//       Name: "X-series",
+//       type: "Melon",
+//       productionDate: "2014-05-11",
+//       color: ["Silver", "black"],
+//       amount: 21,
+//       condition: "Old",
+//       price: 34366729,
+//     };
+//     expect(async () => await AllCars.create()).not.toThrow();
+//   });
 
-          const val = res.body.data.addOrganization;
-          expect(val).toHaveProperty("organization");
-          expect(val).toHaveProperty("ceo");
-          // expect(val).toHaveProperty("marketValue");
-          done()
-        })
-        afterAll(() => {
-          mongoose.connection.close();
-          done();
-        });
-  })
-  
+//   it("can add to staffs", async () => {
+//     let data = {
+//       name: "Freddie Ochukwu",
+//       position: "Manager",
+//       salary: 250000,
+//       homeAddress: "No.4 Behind okija, yola, adamawa state",
+//     };
+//     expect(async () => await Staffs.create()).not.toThrow();
+//   });
 
-})
-    // it("can update organization", () => {
-    //   try {
-    //   request
-    //     .post("/graphql")
-    //     .send({
-    //       query:
-    //         'mutation {updateOrganization(organization:"Pito Bank",products: ["first", "second"],marketValue: "92%", address: "Lacagan", ceo:"Mr Abass", country: "Nigeria", employees: ["Bisi", "Kenny", "Silver"]){organization, products}}',
-    //     })
-    //     .set("Accept", "application.json")
-    //     .expect("Content-Type", /json/)
-    //     .expect(200)
-    //     .end((err, res) => {
-    //       console.log(res);
-    //       if (err) return err.message;
-    //     expect(res.body).toBeInstanceOf{Object}
-    //       const val = res.body.data.addOrganization;
-    //       expect(val).toHaveProperty("organization");
-    //       expect(val).toHaveProperty("ceo");
-    //       expect(val).toHaveProperty("marketValue");
-    //     });
-    //   } catch (error) {
-    //     console.log(`error ${error.toString()}`);
-    //   }
-    // });
+//   it("can update a purchased car", async () => {
+//     expect(async () => await PurchasedCars.findByIdAndUpdate()).not.toThrow();
+//   });
 
-  //   it("can add a purchased car", async () => {
-  //     let data = {
-  //       type: "Toyota",
-  //       modelNumber: "7452MXDGH-2017",
-  //       saleDate: "2020-09-10T14:14:08.670Z",
-  //       buyer: "Olapeju",
-  //       color: "Green",
-  //     };
+//   it("can update a car", async () => {
+//     expect(async () => await AllCars.findByIdAndUpdate()).not.toThrow();
+//   });
 
-  //     expect(async () => await PurchasedCars.create()).not.toThrow();
-  //   });
+//   it("can update a staff", async () => {
+//     expect(async () => await Staffs.findByIdAndUpdate()).not.toThrow();
+//   });
 
-  //   it("can add to cars", async () => {
-  //     let data = {
-  //       Name: "X-series",
-  //       type: "Melon",
-  //       productionDate: "2014-05-11",
-  //       color: ["Silver", "black"],
-  //       amount: 21,
-  //       condition: "Old",
-  //       price: 34366729,
-  //     };
-  //     expect(async () => await AllCars.create()).not.toThrow();
-  //   });
+//   it("can get all purchased cars", async () => {
+//     expect(async () => await PurchasedCars.find()).not.toThrow();
+//   });
 
-  //   it("can add to staffs", async () => {
-  //     let data = {
-  //       name: "Freddie Ochukwu",
-  //       position: "Manager",
-  //       salary: 250000,
-  //       homeAddress: "No.4 Behind okija, yola, adamawa state",
-  //     };
-  //     expect(async () => await Staffs.create()).not.toThrow();
-  //   });
+//   it("can get all cars", async () => {
+//     expect(async () => await AllCars.find()).not.toThrow();
+//   });
 
-  //   it("can update a purchased car", async () => {
-  //     expect(async () => await PurchasedCars.findByIdAndUpdate()).not.toThrow();
-  //   });
+//   it("can get all staffs", async () => {
+//     expect(async () => await Staffs.find()).not.toThrow();
+//   });
 
-  //   it("can update a car", async () => {
-  //     expect(async () => await AllCars.findByIdAndUpdate()).not.toThrow();
-  //   });
+//   it("can delete a purchased car", async () => {
+//     expect(async () => await PurchasedCars.findByIdAndRemove()).not.toThrow();
+//   });
 
-  //   it("can update a staff", async () => {
-  //     expect(async () => await Staffs.findByIdAndUpdate()).not.toThrow();
-  //   });
+//   it("can delete a car", async () => {
+//     expect(async () => await AllCars.findByIdAndRemove()).not.toThrow();
+//   });
 
-  //   it("can get all purchased cars", async () => {
-  //     expect(async () => await PurchasedCars.find()).not.toThrow();
-  //   });
+//   it("can get all staff", async () => {
+//     expect(async () => await Staffs.findByIdAndRemove()).not.toThrow();
+//   });
 
-  //   it("can get all cars", async () => {
-  //     expect(async () => await AllCars.find()).not.toThrow();
-  //   });
+// }
+// })
 
-  //   it("can get all staffs", async () => {
-  //     expect(async () => await Staffs.find()).not.toThrow();
-  //   });
 
-  //   it("can delete a purchased car", async () => {
-  //     expect(async () => await PurchasedCars.findByIdAndRemove()).not.toThrow();
-  //   });
 
-  //   it("can delete a car", async () => {
-  //     expect(async () => await AllCars.findByIdAndRemove()).not.toThrow();
-  //   });
 
-  //   it("can get all staff", async () => {
-  //     expect(async () => await Staffs.findByIdAndRemove()).not.toThrow();
-  //   });
 
-  // }
-  // })
-  
+
+
+
+
